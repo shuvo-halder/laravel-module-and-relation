25b7b17765e2ea86d4d6611751da80fe8fe9fc11
SHUVO HALDER <samshuvo00@gmail.com>
2025-01-30 20:20:14 +0600

    belongsToMany() #1 relation define and the post_tag migration create

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,10 @@ class Post extends Model
             'name' => 'Gueast Author',
         ]);
     }
+
+    public function tags(){
+        // pivot table name basiclly start with name conventional of both table name
+        // in this case post_tag is pivot table, third paramiter and the four paramiter is the column of pivot table
+        return $this->belongsToMany(Tag::class, 'post_tag', 'post_id', 'tag_id');
+    }
 }
